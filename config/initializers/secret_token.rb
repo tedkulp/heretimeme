0a269abcc23f2f6f4f243da49fcfe5f97a2cd13f
Ted Kulp <ted@tedkulp.com>
2011-11-13 12:40:14 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heretimeme::Application.config.secret_token = '9252c10688e785ec79a2f8d7ae8d82b0368c58c0a88cd35bc656401c853c2e3a0d4b802b45c03994f9ff82df5285a5b5ad57da70dbb7593a5a4b18f1bf745349'
